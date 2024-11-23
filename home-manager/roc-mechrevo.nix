b8d6d0865c45024fd55361e91465d5587711d7de
Roc <roc.gui@foxmail.com>
2024-11-23 18:54:22 +0800

    pkgs: dance with nixfmt-rfc-style
    format with nixfmt-rfc-style instead of nixfmt in aur
    add nix.conf for experimental-features
    flake update

--- home-manager/roc-mechrevo.nix
+++ home-manager/roc-mechrevo.nix
@@ -5,7 +5,9 @@
   home = {
     username = "roc";
     homeDirectory = "/home/roc";
-    # packages = with pkgs; [ neofetch ];
+    packages = with pkgs; [
+      nixfmt-rfc-style
+    ];
     file = {
       # zsh
       ".zshrc".source = ./archlinux/zsh/.zshrc;
@@ -15,19 +17,19 @@
       ".gitmessage".source = ./archlinux/git/.gitmessage;
 
       # fontconfig
-      ".config/fontconfig/fonts.conf".source =
-        ./archlinux/fontconfig/fonts.conf;
+      ".config/fontconfig/fonts.conf".source = ./archlinux/fontconfig/fonts.conf;
 
       # rime
-      ".local/share/fcitx5/rime/default.custom.yaml".source =
-        ./archlinux/rime/default.custom.yaml;
+      ".local/share/fcitx5/rime/default.custom.yaml".source = ./archlinux/rime/default.custom.yaml;
 
       # vim
       ".vim/vimrc".source = ./archlinux/vim/vimrc;
+
+      # nix
+      ".config/nix/nix.conf".source = ./archlinux/nix/nix.conf;
     };
     stateVersion = "24.05";
   };
 
   programs.home-manager.enable = true;
 }
-
